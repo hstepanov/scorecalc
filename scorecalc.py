251c251a0080b7e5c4fb97a347ab52148bc695ec
admin <admin@nstepanov.local>
2020-06-18 21:13:08 +0300

    beginning with sqlite

--- scorecalc.py
+++ scorecalc.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-res_dict = {}
-participant = ' '
-while participant != '':
-    participant = input('Input participant name:')
-    res_dict.setdefault(participant, None)
-print(res_dict)
+import sqlite3
+#res_dict = {}
+#participant = ' '
+#while participant != '':
+#    participant = input('Input participant name:')
+#    res_dict.setdefault(participant, None)
+#print(res_dict)
+
+tours = int(input("Input tours number:"))
+conn = sqlite3.connect("scorecalc.db")
+cursor = conn.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS score_table
+              (participant TEXT NOT NULL PRIMARY KEY)
+               """)
+for number in tours:
+    num = int(1)
+    query = 'ALTER TABLE {}
