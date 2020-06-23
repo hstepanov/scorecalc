8a1cb154373a8fd6a28b5fced534f1e05d807d7a
hucho <vonapets.n@gmail.com>
2020-06-23 23:45:08 +0300

    second commit

--- scorecalc.py
+++ scorecalc.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
-res_dict = {}
-participant = ' '
-while participant != '':
-    participant = input('Input participant name:')
-    res_dict.setdefault(participant, None)
-print(res_dict)
+#res_dict = {}
+#participant = ' '
+#while participant != '':
+#    participant = input('Input participant name:')
+#    res_dict.setdefault(participant, None)
+#print(res_dict)
+
+import sqlite3
+
+conn = sqlite3.connect("scorecalc.db")
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS
+                  participants(id int PRIMARY KEY, name text NOT NULL)
+               """)
+conn.commit()
+
+cursor.execute("SELECT * FROM participants")
+
+conn.close()
