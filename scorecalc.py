06594b607af4cc845b24509f13df8c4de1807e67
hstepanov <vonapets.n@gmail.com>
2020-06-24 14:19:07 +0300

    commit from phone

--- scorecalc.py
+++ scorecalc.py
@@ -1,18 +1,33 @@
-#!/usr/bin/env python3
 import sqlite3
-#res_dict = {}
-#participant = ' '
+#part_dict = {}
+#participant = None
+#''' whitespase is the end of input'''
 #while participant != '':
-#    participant = input('Input participant name:')
-#    res_dict.setdefault(participant, None)
-#print(res_dict)
-
-tours = int(input("Input tours number:"))
+#  participant = input('input name:')
+#  if participant.isalpha():
+#    part_dict.setdefault(participant, None)
+#  else:
+#    break
+#
+#print(part_dict)
+tours_num = int(input("Input number of tours:"))
 conn = sqlite3.connect("scorecalc.db")
-cursor = conn.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS score_table
-              (participant TEXT NOT NULL PRIMARY KEY)
+cursor = con.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS scorecalc
+                  (id integer NOT NULL PRIMARY KEY, 
+                   participant text, 
+                   first_t integer, second_t integer)
                """)
-for number in tours:
-    num = int(1)
-    query = 'ALTER TABLE {}
+conn.commit()
+
+def add_participant(part_name):
+  first_t, second_t = 0
+  id = 1
+  while part_name.isalpha():
+    cursor.execute("""INSERT INTO scorecalc
+                      VALUES (?, ?, ?, ?);
+                   """)
+    conn.commit()
+    id += 1
+    
+conn.close()
